5d06a34ab11032dcabc04c46eac286e61f4615f6
mr.scrpt <mr.scrpt@gmail.com>
2024-10-24 16:31:23 +0300

    refactor: comments

--- lua/html_to_scss_skelet/init.lua
+++ lua/html_to_scss_skelet/init.lua
@@ -1,6 +1,5 @@
 local M = {}
 
--- Функция для извлечения имен классов из строки
 local function extract_class_names(line)
 	local class_names = {}
 	-- Поиск class="..."
@@ -10,7 +9,6 @@ local function extract_class_names(line)
 			table.insert(class_names, class_name)
 		end
 	end
-	-- Поиск class='...'
 	for class_attr in string.gmatch(line, "class%s*=%s*'([^']+)'") do
 		for class_name in string.gmatch(class_attr, "%S+") do
 			table.insert(class_names, class_name)
@@ -19,21 +17,18 @@ local function extract_class_names(line)
 	return class_names
 end
 
--- Основная функция для генерации SCSS-скелета
 function M.generate_scss_skeleton()
 	-- Получение выделенного текста
 	local s_start = vim.fn.getpos("'<")
 	local s_end = vim.fn.getpos("'>")
 	local lines = vim.fn.getline(s_start[2], s_end[2])
 
-	-- Если выделена одна строка, корректируем колонки
 	if #lines == 1 then
 		local start_col = s_start[3]
 		local end_col = s_end[3]
 		lines[1] = string.sub(lines[1], start_col, end_col)
 	end
 
-	-- Сбор имен классов
 	local class_set = {}
 	for _, line in ipairs(lines) do
 		local class_names = extract_class_names(line)
@@ -42,7 +37,6 @@ function M.generate_scss_skeleton()
 		end
 	end
 
-	-- Определение базового класса
 	local base_classes = {}
 	for class_name, _ in pairs(class_set) do
 		if not string.find(class_name, "__") then
@@ -54,7 +48,6 @@ function M.generate_scss_skeleton()
 	if #base_classes == 1 then
 		base_class = base_classes[1]
 	else
-		-- Если несколько базовых классов, выбираем первый из HTML
 		for _, line in ipairs(lines) do
 			local class_names = extract_class_names(line)
 			for _, class_name in ipairs(class_names) do
@@ -70,18 +63,15 @@ function M.generate_scss_skeleton()
 	end
 
 	if not base_class then
-		-- Если базовый класс не найден, берем первый класс
 		for class_name, _ in pairs(class_set) do
 			base_class = class_name
 			break
 		end
 	end
 
-	-- Генерация SCSS-скелета
 	local scss_lines = {}
 	table.insert(scss_lines, "." .. base_class .. " {")
 
-	-- Сбор элементов (классы с '__')
 	local elements = {}
 
 	for class_name, _ in pairs(class_set) do
@@ -94,23 +84,18 @@ function M.generate_scss_skeleton()
 		end
 	end
 
-	-- Добавление элементов
 	for _, element in ipairs(elements) do
 		table.insert(scss_lines, "  &" .. "__" .. element .. " {")
 		table.insert(scss_lines, "  }")
 		table.insert(scss_lines, "")
 	end
 
-	-- Закрытие базового класса
 	table.insert(scss_lines, "}")
 
-	-- Объединение строк SCSS
 	local scss_text = table.concat(scss_lines, "\n")
 
-	-- Копирование в буфер обмена
 	vim.fn.setreg("+", scss_text)
 
-	-- Вывод сообщения
 	print("SCSS has been generated and copied to clipboard.")
 end
 
